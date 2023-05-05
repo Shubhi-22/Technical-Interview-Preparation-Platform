1c95c35cc3c8279653b910409be8833ab10c2c1d
Shubhra Srivastava <srivastavashubhra35@gmail.com>
2023-05-05 13:36:02 +0530

    commit 2

--- src/Pages/toggle.js
+++ src/Pages/toggle.js
@@ -1,7 +1,7 @@
-(function (){
-        $('navbar-toggler').click(function(){
-            $(this).toggleClass('active');
-            return $('collapse').toggleClass('open');
-        });
+// (function (){
+//         $('navbar-toggler').click(function(){
+//             $(this).toggleClass('active');
+//             return $('collapse').toggleClass('open');
+//         });
 
-}).call(this);
+// }).call(this);
