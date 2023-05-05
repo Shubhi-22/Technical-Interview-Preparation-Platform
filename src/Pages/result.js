eab7ba070f26313c4b0c0718dd8248b7a92a822b
Shubhra Srivastava <srivastavashubhra35@gmail.com>
2023-05-05 14:58:14 +0530

    delete

--- src/Pages/result.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const result =()=>{
-    return <>
-    <h1>Result</h1>
-    </>
-}
-
-export default result;
--- src/Pages/toggle.js
+++
@@ -1,7 +0,0 @@
-// (function (){
-//         $('navbar-toggler').click(function(){
-//             $(this).toggleClass('active');
-//             return $('collapse').toggleClass('open');
-//         });
-
-// }).call(this);
